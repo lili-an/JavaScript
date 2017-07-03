440d02cf6099ceac026e4e86cb3f8e5c15fd5dba
strong <70786564@qq.com>
2017-07-03 14:55:12 +0800

    'modify'

--- phantomjs/step_github/signIn_github.js
+++ phantomjs/step_github/signIn_github.js
@@ -1,4 +1,4 @@
-var Promise = require('es6-promise-polyfill').Promise;
+// var Promise = require('es6-promise-polyfill').Promise;
 
 var page = require('webpage').create(),
     testindex = 0,
@@ -23,7 +23,7 @@ var steps = [
         page.evaluate(function(obj) {
             var form = document.getElementsByTagName('form')[0];
             var btn = document.getElementsByClassName('btn-block')[0];
-            form.elements["login_field"].value = '929420309@qq.com';
+            form.elements["login_field"].value = '707865641@qq.com';
             form.elements["password"].value = 'wang0000';
             btn.click();
 
@@ -36,19 +36,71 @@ var steps = [
         page.render('login-succ-github.png');
     },
 
+    // function() {
+    //     page.evaluate(function(obj) {
+
+    //         document.getElementsByClassName('dropdown-item')[4].click();
+
+    //         return document.title;
+    //     });
+    //     loadInProgress = true;
+    // },
+
+    //  function() {
+    //     page.render('login-succ1-github.png');
+    // },
+
+    function() {
+      page.open("https://github.com/lili-an/zhihu-daily");
+    },
+
+    function() {
+        page.render('login-succ2-github.png');
+    },
+
     function() {
         page.evaluate(function(obj) {
 
-            document.getElementsByClassName('dropdown-item')[4].click();
+            document.getElementsByClassName('js-toggler-target')[0].click();
 
             return document.title;
         });
         loadInProgress = true;
     },
 
-     function() {
-        page.render('login-succ1-github.png');
+    function() {
+        page.render('login-star-github.png');
     },
+
+    // function() {
+    //     page.evaluate(function(obj) {
+
+    //         document.getElementsByClassName('js-site-search-focus')[0].value = 'lili-an';
+    //         document.getElementsByClassName('js-site-search-form')[0].submit();
+
+    //         return document.title;
+    //     });
+    //     loadInProgress = true;
+    // },
+
+    //  function() {
+    //     page.render('login-search-github.png');
+    // },
+
+    //  function() {
+    //     page.evaluate(function(obj) {
+
+    //         document.getElementsByClassName('js-site-search-focus')[0].value = 'lili-an';
+    //         document.getElementsByClassName('js-site-search-form')[0].submit();
+
+    //         return document.title;
+    //     });
+    //     loadInProgress = true;
+    // },
+
+    //  function() {
+    //     page.render('login-searchResult-github.png');
+    // },
 ];
 
 var interval = setInterval(function() {
